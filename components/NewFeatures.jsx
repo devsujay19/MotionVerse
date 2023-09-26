150d6d94cdd5a0aa5aef95ac8716951863bb9bf6
Sujay Mukherjee <sujay.mukherjeedev@gmail.com>
2023-09-26 21:33:48 +0530

    initial commit

---
+++ components/NewFeatures.jsx
@@ -0,0 +1,20 @@
+import styles from '../styles';
+
+const NewFeatures = ({ imgUrl, title, title2, subtitle }) => (
+  <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
+    <div
+      className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]`}
+    >
+      <img src={imgUrl} alt="icon" className="w-1/2 h-1/2 object-contain" />
+    </div>
+    <h1 className="mt-[26px] font-bold text-[24px] leading-[30.24px] text-white">
+      Get {title}
+      {title2}
+    </h1>
+    <p className="flex-1 mt-[16px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]">
+      {subtitle}
+    </p>
+  </div>
+);
+
+export default NewFeatures;
---
+++ constants/index.js
@@ -0,0 +1,91 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Default World',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'The Planet of Sanity',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'The Place of Islands',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'The Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that make interest inside you and you want to enter.',
+  'Enter the world and find or invite your friends on that world.',
+  'Just stay on the gas and have fun on your experience.',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: ' a chance to name new worlds',
+    subtitle:
+        'We have just realesed the latest update with new world and naming features for you to try and have fun!',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title2: 'more realistic experiences',
+    subtitle:
+        'In the latest update of our MotionVerse project, your eyes are narrow, making the world more realistic than ever for you to try it out.',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'The launching event of the MotionVerse project made our users mad!',
+    subtitle:
+        'In this year 2023, the launching event of our big and loved project MotionVerse was made our every users mad and they started trying it immediately! That was a memorable event of all time for our users...',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 tips that will make your MotionVerse experiences more better than ever you had!',
+    subtitle:
+        '1. Be mindful of your surroundings: The MotionVerse can be a disorienting place, so it\'s important to be aware of your surroundings and where you\'re going...',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Make creation that can not be destroyed until you want!',
+    subtitle:
+        'Do you know? With the latest update, you can make creations (anything) you want to create and anyone won\'t be able to destroy it until you want that...',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+    href: 'https://www.twitter.com/devsujay19',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+    href: 'https://www.instagram.com/devsujay19',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+    href: 'https://www.facebook.com/people/Sujay-Mukherjee/100092647425033/',
+  },
+];
